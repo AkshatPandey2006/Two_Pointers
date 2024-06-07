75a615088c2f9e4f22c20dfdaf88b02598d2eeae
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-06-07 20:01:56 +0530

    Create Reverse_Word_in_a_String.cpp

---
+++ Reverse_Word_in_a_String.cpp
@@ -0,0 +1,14 @@
+string reverseWords(string s) {
+    int lastSpace = 0;
+    
+    for(int i=0; i<s.length(); i++) {
+        if(s[i] == ' ') {
+            reverse(s.begin() + lastSpace, s.begin() + i);
+            lastSpace = i + 1;
+        }
+    }
+    
+    reverse(s.begin() + lastSpace, s.end());
+    return s;
+}
+
