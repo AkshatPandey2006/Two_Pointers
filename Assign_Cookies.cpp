80109f4f5baebf5af18030423253d0d11b22a198
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-06-08 22:35:22 +0530

    Update Assign_Cookies.cpp

--- Assign_Cookies.cpp
+++ Assign_Cookies.cpp
@@ -1,6 +1,7 @@
 int findContentChildren(vector<int>& g, vector<int>& s) {
     sort(g.begin(), g.end());
     sort(s.begin(), s.end());
+    
     int gIndx = 0, gSz = g.size(), sSz = s.size();
     
     for(int indx = 0; indx < sSz; indx++){
